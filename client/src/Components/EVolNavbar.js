1e1634fc2ae031d2967e2a1af97cd43543cb5b86
LuisGSandoval <luis.sandoval.457@outlook.com>
2020-02-08 18:35:06 -0500

    Se agrega una opcion de ver el correo de usuarios

--- client/src/Components/EVolNavbar.js
+++ client/src/Components/EVolNavbar.js
@@ -3,11 +3,17 @@ import { Navbar, NavbarBrand, NavbarText } from 'reactstrap';
 
 import { logout } from '../Actions/AuthActions';
 
+let user = window.localStorage.userData;
+user = JSON.parse(user);
+
 const EVolNavbar = () => {
   return (
     <div>
       <Navbar color="light" className="border-bottom border-danger" light>
-        <NavbarBrand>e volution test</NavbarBrand>
+        <NavbarBrand>remembrant</NavbarBrand>
+        <NavbarText className="cursor-pointer" onClick={logout}>
+          {user.email.split('@')[0]}
+        </NavbarText>
         <NavbarText className="cursor-pointer" onClick={logout}>
           Salir
         </NavbarText>
